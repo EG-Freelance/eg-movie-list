7a5029c2a2efa98eb196363641f4c44284c56a34
Erik <flyers_rock_87@hotmail.com>
2019-05-14 18:46:43 +0000

    customize tooltips and add episode links

--- app/assets/javascripts/charts/charts.js
+++ app/assets/javascripts/charts/charts.js
@@ -30,7 +30,6 @@ angular.module('EgMovieList.Charts', [
   chartsCtrl.series_list = {};
   
   function init() {
-    // organize_chart_data(chartsCtrl.series_list)
   } // end of init
   
   init();
@@ -111,15 +110,85 @@ angular.module('EgMovieList.Charts', [
     })
   }
   
+  // var customTooltips = function(tooltip) {
+  // 	// Tooltip Element
+  // 	var tooltipEl = document.getElementById('chartjs-tooltip');
+  
+  // 	if (!tooltipEl) {
+  // 		tooltipEl = document.createElement('div');
+  // 		tooltipEl.id = 'chartjs-tooltip';
+  // 		tooltipEl.innerHTML = '<table></table>';
+  // 		this._chart.canvas.parentNode.appendChild(tooltipEl);
+  // 	}
+  
+  // 	// Hide if no tooltip
+  // 	if (tooltip.opacity === 0) {
+  // 		tooltipEl.style.opacity = 0;
+  // 		return;
+  // 	}
+  
+  // 	// Set caret Position
+  // 	tooltipEl.classList.remove('above', 'below', 'no-transform');
+  // 	if (tooltip.yAlign) {
+  // 		tooltipEl.classList.add(tooltip.yAlign);
+  // 	} else {
+  // 		tooltipEl.classList.add('no-transform');
+  // 	}
+  
+  // 	function getBody(bodyItem) {
+  // 		return bodyItem.lines;
+  // 	}
+  
+  // 	// Set Text
+  // 	if (tooltip.body) {
+  // 		var titleLines = tooltip.title || [];
+  // 		var bodyLines = tooltip.body.map(getBody);
+  
+  // 		var innerHtml = '<thead>';
+  
+  // 		titleLines.forEach(function(title) {
+  // 			innerHtml += '<tr><th>' + title + 'ABCABCABC</th></tr>';
+  // 		});
+  // 		innerHtml += '</thead><tbody>';
+  
+  // 		bodyLines.forEach(function(body, i) {
+  // 			var colors = tooltip.labelColors[i];
+  // 			var style = 'background:' + "#303030";
+  // 			style += '; border-color:' + "#909090";
+  // 			style += '; border-width: 2px';
+  // 			var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+  // 			innerHtml += '<tr><td>' + span + body + '</td></tr>';
+  // 		});
+  // 		innerHtml += '</tbody>';
+  
+  // 		var tableRoot = tooltipEl.querySelector('table');
+  // 		tableRoot.innerHTML = innerHtml;
+  // 	}
+  
+  // 	var positionY = this._chart.canvas.offsetTop;
+  // 	var positionX = this._chart.canvas.offsetLeft;
+  
+  // 	// Display, position, and set styles for font
+  // 	tooltipEl.style.opacity = 0.5;
+  // 	tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+  // 	tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+  // 	tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+  // 	tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
+  // 	tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+  // 	tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+  // };
+  
   function organize_chart_data(raw){
     var seasons = Object.keys(raw).sort(function(e){ return parseInt(e) });
     // var data = [];
     var series_labels = [];
     var labels = [];
     var datasets = [];
+    var ep_data = [];
     // create an empty dataset for each season
     seasons.forEach(function(){
       datasets.push([]);
+      ep_data.push([]);
     })
     
     // for each season
@@ -131,9 +200,11 @@ angular.module('EgMovieList.Charts', [
         // push in imdb rating value if correct dataset -- otherwise, push NaN
         for(var i = 0; i < seasons.length; i++){
           if(i + 1 == parseInt(s)){
-            datasets[i].push(parseFloat(e['imdbRating']))
+            datasets[i].push(parseFloat(e['imdbRating']));
+            ep_data[i].push(e);
           }else{
             datasets[i].push(NaN);
+            ep_data[i].push("");
           }
         }
       });
@@ -142,6 +213,8 @@ angular.module('EgMovieList.Charts', [
     chartsCtrl.dataset = datasets;
     chartsCtrl.labels = labels;
     chartsCtrl.series_labels = series_labels;
+    
+    var link_base = "https://www.imdb.com/title/"
     
     chartsCtrl.options = { 
       elements: {
@@ -152,27 +225,47 @@ angular.module('EgMovieList.Charts', [
       legend: {
         display: true
       },
-      title: {
-        display: true,
-        text: chartsCtrl.chart_title
-      },
-      tooltips: {
-        mode: 'label',
+      // title: {
+      //   display: true,
+      //   text: chartsCtrl.chart_title,
+      //   fontSize: 30
+      // },
+			tooltips: {
+        callbacks: {
+          title: function(tooltipItem) {
+            return "Season " + seasons[tooltipItem[0].datasetIndex] + ' Episode ' + ep_data[tooltipItem[0].datasetIndex][tooltipItem[0].index]["Episode"];
+          },
+          beforeLabel: function(tooltipItem){
+            return "Title: " + ep_data[tooltipItem.datasetIndex][tooltipItem.index]["Title"];
+          },
+          label: function(tooltipItem){
+            return "Score: " + ep_data[tooltipItem.datasetIndex][tooltipItem.index]["imdbRating"];
+          }
+        }
       },
       scales: { 
         xAxes: [{ 
           type: 'category', 
-          labels: chartsCtrl.labels 
+          labels: chartsCtrl.labels
         }],
         yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "IMDb Rating"
+          },
           ticks: {
             min: 0,
             max: 10
           }
         }]
-      } 
+      },
+      onClick: function(ev, el){
+        var el = el[0];
+        // if not clicking on an element
+        if(!el){ return; }
+        var ep_url = ep_data[el._datasetIndex][el._index]["imdbID"]
+        $window.open(link_base + ep_url, '_blank');
+      }
     }
-
   }
-  
 }]);
